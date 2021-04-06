92bab41f0ef869c14ba52ff6ea5567c27c2efb9b
cungnv <cungnv@hblab.vn>
2021-04-06 11:23:06 +0700

    vue hour part one

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,46 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Menu/>
+    <!-- 
+      the input va div in ra cung 1 du lieu la name. Vue co chưc năng 2 ways binding. Ở data ở tầng dưới
+      thay đổi thì tầng trên thay đổi và ngược lại. Ví dụ nếu thay đổi name ở file nay thì in ra cũng thay đổi.
+      Do name là dữ liệu được dùng ở cả thẻ div và input nên khi ta viết thay đổi trong input thì name trong thẻ
+      div cũng thay đổi theo mặc dù ta k thay đổi giá trị của input ->Thay đổi theo chiều từ trên xuống dưới
+      -> Điểm khác nhau cả vue và react
+     -->
+    <input type='text' v-model='name'>
+    <!--
+      tên v-show phải đặt như thế do là thuộc tính của vue. Nếu true thì hiện, k thì thôi
+    -->
+    <div v-bind:id='id' v-show='isShow'>Made by {{name}}</div>
+    <button @click="isShow=!isShow">Click to hidden or show</button>
+    <input type='text' v-model='name' :disabled='iputDisable' :class="{text_red:iputDisable}">
+    <button @click="iputDisable=!iputDisable">Open/Close</button>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Menu from './components/Menu.vue'
 
 export default {
-  name: 'App',
+  // ham data se tra ve mot obj chua du lieu ta muon xu ly. Ví dụ khi get api thì để data vào biến name
+  //hoặc tên biến nào đấy rồi ở trên lấy xử lý
+  data(){
+    return {
+      name:'Nguyen Van A',
+      isShow:true,
+      id:'test',
+      xyz:'hello',
+      iputDisable:true,
+    }
+  },
+  /**
+    Nhìn vào sơ đồ lifecycle nếu muốn viết sự kiện nào thì override lại. Ví dụ trước khi data thay đổi muốn làm
+    gì đấy thì viết lại hàm beforeUpdate(). Cứ viết lạ đúng tên hàm vào đúng thời điểm mà ta muôn.
+  */
   components: {
-    HelloWorld
+    Menu
   }
 }
 </script>
@@ -24,5 +53,8 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.text_red{
+  background-color:#2ABE3A
 }
 </style>
---
+++ src/components/Menu.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="menu">
+   <h2>This is menu</h2>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style scoped>
+
+</style>
